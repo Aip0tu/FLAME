9a402a2f7b6c5394da477ddb74e69468974e4d3c
xiaozhangxin <2351788625@qq.com>
2026-03-24 17:00:45 +0800

    单分子预测

--- FLAME/scripts/pred_flmpnn.py
+++ FLAME/scripts/pred_flmpnn.py
@@ -9,8 +9,10 @@ if __name__ == '__main__':
         for model in [ 'FluoDB']:
             for target in targets:
                 model_path = f'model/flsf/{model}_{target}/fold_0/model_0'
-                input_file = f'data/{data_base}/{target}_test.csv'
-                output_file = f'pred/{data_base}/flsf{tag}_{model}_{target}.csv'
+                #input_file = f'data/{data_base}/{target}_test.csv'
+                #output_file = f'pred/{data_base}/flsf{tag}_{model}_{target}.csv'
+                input_file = f'data/{data_base}/pre.csv'
+                output_file = f'pred/{data_base}/flsf{tag}_{model}_{target}_1.csv'
                 if not os.path.exists(f'pred/{data_base}/'):
                     os.makedirs(f'pred/{data_base}/')
                 flsf_predict(model_path, output_file, input_file=input_file)
